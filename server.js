495c7eb89824844eba02de5aaea021312a7828dd
unknown <snehagurav14.sg@gmail.com>
2020-04-13 22:42:58 +0530

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-const app = require("./app")
+let express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const userRoutes = require("./Routes/userRoutes")
+const propertyRoutes = require("./Routes/propertyRoutes")
 const PORT = PROCESS.ENV.PORT || 5000
+require("./db")
+require("./utils/cloudinary")
+const app = express()
+app.use(express.json())
+app.use(express.static("uploads"))
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+app.use(userRoutes)
+app.use(propertyRoutes)
+
+
+
+
+
+
+
+//const app = require("./app")
+
 app.listen(PORT , () => {
     console.log("server started")
 })
